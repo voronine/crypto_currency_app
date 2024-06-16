0e1dfa2f7c5f7144b654975d052e5927567c967e
voronine <voronine@ukr.net>
2024-06-16 16:49:28 +0300

    render!!

--- currency-app-server/server.js
+++ currency-app-server/server.js
@@ -38,8 +38,8 @@ app.use('/api/currency', currencyRoutes);
 const valueRoutes = require('./routes/value');
 app.use('/api/value', valueRoutes);
 
-// app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
