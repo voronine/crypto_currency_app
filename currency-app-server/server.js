6553305760d64e880a07d5f1d346d5f7b2ec7569
voronine <voronine@ukr.net>
2024-06-16 18:27:51 +0300

    render2

--- currency-app-server/server.js
+++ currency-app-server/server.js
@@ -35,11 +35,11 @@ const valueRoutes = require('./routes/value');
 app.use('/api/value', valueRoutes);
 
 // Статические файлы из папки 'build' клиентского приложения
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 // Отправка 'index.html' для всех остальных маршрутов
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
