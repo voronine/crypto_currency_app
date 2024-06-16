ecb9da8f52f8ee6e5e6c62847bab37bb70fd408c
voronine <voronine@ukr.net>
2024-06-16 14:24:15 +0300

    dependes+server_new

--- currency-app-server/server.js
+++ currency-app-server/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'https://www.gstatic.com'],
+    },
+  }));
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
